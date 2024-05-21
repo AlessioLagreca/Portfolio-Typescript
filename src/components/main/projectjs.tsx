295452dd02932ecd0d4b1c679705b83a7e99d2c7
Alessio Lagreca <82826676+AlessioLagreca@users.noreply.github.com>
2024-05-21 17:20:21 +0200

    added new project to projects section

--- src/components/main/projectjs.tsx
+++ src/components/main/projectjs.tsx
@@ -8,20 +8,41 @@ const Project = (): JSX.Element => {
 				<ul>
 					<Separator />
 
-					<li className='flex gap-2 items-center'>
-						<div className='rounded-full bg-orange-300 w-2 h-2'></div>
-						<Link
-							className='py-2 hover:underline hover:text-gray-600'
-							href='https://password-manager-tan-phi.vercel.app/'
-						>
-							Password Manager 🗝️🗝️🗝️
-						</Link>
+					<li className='flex flex-col'>
+						<div className='flex gap-2 items-center'>
+							<div className='rounded-full bg-orange-300 w-2 h-2'></div>
+							<Link
+								className='py-2 hover:underline hover:text-gray-600'
+								href='https://password-manager-tan-phi.vercel.app/'
+								passHref
+								legacyBehavior
+							>
+								<a target='_blank'>Password Manager 🗝️🗝️🗝️</a>
+							</Link>
+						</div>
+						<span className='pl-4 py-2 text-gray-500'>
+							Password Manager i've build using Next.js, Tailwind CSS, Crypto Node module for encryption and Postgres as
+							database
+						</span>
 					</li>
 					<Separator />
 
-					<li className='flex gap-2 items-center'>
-						<div className='rounded-full bg-orange-300 w-2 h-2'></div>
-						<span className='py-2'>Really cool beautiful Project</span>
+					<li className='flex flex-col'>
+						<div className='flex gap-2 items-center'>
+							<div className='rounded-full bg-orange-300 w-2 h-2'></div>
+							<Link
+								className='py-2 hover:underline hover:text-gray-600'
+								href='https://landing-page-shadcn-clerk.vercel.app/'
+								passHref
+								legacyBehavior
+							>
+								<a target='_blank'>Landing Page (FrontEnd-Mentor) 🚀🚀🚀</a>
+							</Link>
+						</div>
+						<span className='pl-4 py-2 text-gray-500'>
+							Project by frontendmentor.io i've built using using Next.js, Tailwind CSS, TypeScript. For animation i've
+							used Framer Motion, Lenis Scroll and GSAP and Clerk for Authentication
+						</span>
 					</li>
 				</ul>
 			</div>
